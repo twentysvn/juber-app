16ecffc321d4c991697074ec1aef84ac0ed3254c
twenty seven <41100127+twentysvn@users.noreply.github.com>
2021-03-05 14:59:53 +0700

    init

---
+++ app/Http/Controllers/CabCustomerController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\cab_customer;
+use Illuminate\Http\Request;
+
+class CabCustomerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\cab_customer  $cab_customer
+     * @return \Illuminate\Http\Response
+     */
+    public function show(cab_customer $cab_customer)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\cab_customer  $cab_customer
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(cab_customer $cab_customer)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\cab_customer  $cab_customer
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, cab_customer $cab_customer)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\cab_customer  $cab_customer
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(cab_customer $cab_customer)
+    {
+        //
+    }
+}
